0ee2f943737db7c5d7f0dbaab54acfe1160cd084
Cash Kiefer <cash.kiefer2008@gmail.com>
2025-04-28 10:45:55 -0500

    Images, a touch of CSS, and a Confirmation Page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin # type: ignore
-from django.urls import path # type: ignore
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-from django.contrib import admin # type: ignore
-from django.urls import path, include # type: ignore
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('salon.urls')),  # Point to your app
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
